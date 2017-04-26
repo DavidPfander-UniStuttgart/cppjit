348c7ab1f0bbc34143e18f4b1d790be6c0c0bace
fire <fire@element.com>
2017-04-26 23:02:44 +0200

    still bullshit version

--- src/example_inline.cpp
+++ src/example_inline.cpp
@@ -4,7 +4,7 @@
 #include "cppjit/cppjit.hpp"
 #include "example_kernels/kernel_inline.hpp"
 
-CPPJIT_DECLARE_KERNEL(void, test_kernel, (void))
+CPPJIT_DECLARE_KERNEL(void, test_kernel, (int input))
 
 int main(void) {
 
@@ -20,32 +20,36 @@ int main(void) {
   // });
 
   CPPJIT_REGISTER_KERNEL_SOURCE("test_kernel",
-				"#include <iostream> \n extern \"C\" void test_kernel() { std::cout << \"test test kernel\" << std::endl;}");
+                                "#include <iostream> \n extern \"C\" void "
+                                "test_kernel(int input) { std::cout << \"test "
+                                "test kernel: \" << input << std::endl;}");
 
-//     std::string my_kernel_inline_source(
-//         "#include <iostream>\n"
-//         "extern \"C\" void my_inline_kernel() {\n"
-//         "std::cout << \"in inline kernel\" << std::endl;\n"
-//         "}\n");
+  //     std::string my_kernel_inline_source(
+  //         "#include <iostream>\n"
+  //         "extern \"C\" void my_inline_kernel() {\n"
+  //         "std::cout << \"in inline kernel\" << std::endl;\n"
+  //         "}\n");
 
-// cppjit::detail::compile_inline_kernel(my_kernel_inline_source,
-//                                       "my_inline_kernel");
+  // cppjit::detail::compile_inline_kernel(my_kernel_inline_source,
+  //                                       "my_inline_kernel");
 
-// std::cout << "calling kernel (1)..." << std::endl;
-// my_kernel_inline();
+  // std::cout << "calling kernel (1)..." << std::endl;
+  // my_kernel_inline();
 
-// std::cout << "calling kernel (2)..." << std::endl;
-// my_kernel_inline();
+  // std::cout << "calling kernel (2)..." << std::endl;
+  // my_kernel_inline();
 
-// std::cout << "close kernel..." << std::endl;
-// // close_kernel_library();
+  // std::cout << "close kernel..." << std::endl;
+  // // close_kernel_library();
 
-std::cout << "new kernel..." << std::endl;
-test_kernel();
+  std::cout << "new kernel..." << std::endl;
+  test_kernel(3);
+  test_kernel(7);
+  test_kernel(15);
 
-cppjit::finalize();
+  cppjit::finalize();
 
-return 0;
+  return 0;
 }
 
-CPPJIT_DEFINE_KERNEL(void, test_kernel, (void), ())
+CPPJIT_DEFINE_KERNEL(void, test_kernel, (int input), (input))
