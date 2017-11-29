977e5a83a7345153b18c1bc62ac0aeabe8f0b17a
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-11-29 17:03:47 +0100

    fixes for Circle CI

---
+++ scripts/build-cmake.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+set -e
+
+if [ ! -d cmake_install ] ; then
+    wget https://cmake.org/files/v3.9/cmake-3.9.3-Linux-x86_64.tar.gz
+    tar xf cmake-3.9.3-Linux-x86_64.tar.gz
+    mv cmake-3.9.3-Linux-x86_64 cmake_install
+fi
