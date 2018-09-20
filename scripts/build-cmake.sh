33d81106afa96a8912ec6d5200ef3e0a643a76ae
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2018-09-20 18:14:52 +0200

    using cmake 3.12.2 everywhere

--- scripts/build-cmake.sh
+++ scripts/build-cmake.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 
 if [ ! -d cmake_install ] ; then
-    wget https://cmake.org/files/v3.9/cmake-3.9.3-Linux-x86_64.tar.gz
-    tar xf cmake-3.9.3-Linux-x86_64.tar.gz
-    mv cmake-3.9.3-Linux-x86_64 cmake_install
+    wget https://cmake.org/files/v3.12/cmake-3.12.2-Linux-x86_64.tar.gz
+    tar xf cmake-3.12.2-Linux-x86_64.tar.gz
+    mv cmake-3.12.2-Linux-x86_64 cmake_install
 fi
